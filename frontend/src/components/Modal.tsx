6d6d54f718711d32e722b056efe62574a65d7d08
Lere <leredsb10@gmail.com>
2025-03-25 23:02:04 -0300

    fix opacityModal

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -30,7 +30,7 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-[0.3] flex items-center justify-center z-50"
+      className="fixed inset-0 bg-black bg-opacity-1 flex items-center justify-center z-50"
       onClick={onClose}
     >
       <div
